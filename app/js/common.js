7cb03bb776d2bbfd843e0df2d394064eb587df60
Павел Хрипунов <khripunovpp@gmail.com>
2019-08-12 19:52:37 +0300

    both/ add opinionMain sections

--- app/js/common.js
+++ app/js/common.js
@@ -185,20 +185,39 @@ $(function() {
         slidesToShow: 5,
         slidesToScroll: 1,
         responsive: [{
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
+        ]
     })
 
-
+    $('.opinionMain__list').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
 });
